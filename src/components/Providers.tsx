79624fe0888ce5283afb869308405fe53b84e9cd
ExoskyCode <exoskyv2@gmail.com>
2023-04-18 18:34:15 +0200

    ✨ Providers

---
+++ src/components/Providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+type ProvidersProps = {
+	children: ReactNode;
+};
+
+export const Providers = ({ children }: ProvidersProps) => {
+	return (
+		<>
+			<Toaster position='top-center' reverseOrder={false} />
+			{children}
+		</>
+	);
+};
