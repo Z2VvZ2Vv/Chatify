7e89074a3ad3854ece76b1fe8250aef403b5e5e8
ExoskyCode <exoskyv2@gmail.com>
2023-04-18 12:20:53 +0200

    💄 Layout body style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import styles from './main.module.scss'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+		<html lang='en'>
+			<body className={styles.body}>{children}</body>
+		</html>
+  );
 }
