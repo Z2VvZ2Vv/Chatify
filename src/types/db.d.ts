cc1dd08101c1f8d679feb05345318022f6d8d412
ExoskyCode <exoskyv2@gmail.com>
2023-04-19 16:27:15 +0200

    🗃️ db types

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -2,3 +2,16 @@ type User = {
 	email: string;
 	id: string;
 };
+
+type Chat = {
+	id: string,
+	messages: Message[]
+}
+
+type Message = {
+	id: string,
+	senderId: string,
+	receiverId: string,
+	text: string,
+	timestamp: number
+}
