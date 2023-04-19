013378143327159591505e9d56a2041f646af9f1
ExoskyCode <exoskyv2@gmail.com>
2023-04-19 23:46:38 +0200

    💄🧿dashboard requests page

---
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -0,0 +1,31 @@
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
+import {notFound} from "next/navigation";
+import {fetchRedis} from "@/helpers/redis";
+import FriendRequests from "@/components/FriendRequests";
+
+const page = async ({}) => {
+    const session = await getServerSession(authOptions)
+    if(!session) notFound()
+
+    const incomingSendersId = await fetchRedis('smembers', `user:${session.user.id}:incoming_friend_requests`) as string[];
+
+    const incomingFriendRequests = await Promise.all(incomingSendersId.map(async (senderId) => {
+        const sender = await fetchRedis('get', `user:${senderId}`) as string;
+        const senderParsed = JSON.parse(sender) as User
+        return {
+            senderId,
+            senderEmail: senderParsed.email
+        }
+    }))
+    return (
+        <main className="pt-8">
+            <h1 className={'font-bold text-5xl mb-8'}>Demandes d'ami</h1>
+            <div className={'flex flex-col gap--4'}>
+                <FriendRequests sessionId={session.user.id} incomingFriendRequests={incomingFriendRequests}/>
+            </div>
+        </main>
+    )
+}
+
+export default page
