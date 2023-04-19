178ffa81cafa0c74a7d784bb080fff3ec53fe955
ExoskyCode <exoskyv2@gmail.com>
2023-04-19 16:40:41 +0200

    💄 Dashboard add

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -2,8 +2,8 @@ import AddFriendButton from '@/components/AddFriendButton'
 
 const page = () => {
     return (
-        <main className='pt-8'>
-            <h1 className='font-bold text-5xl mb-8'>Ajouter un ami</h1>
+        <main className='pt-8 mt-56 m-auto'>
+            <h1 className='font-bold text-3xl mb-8'>Ajouter un ami</h1>
             <AddFriendButton />
         </main>
     )
