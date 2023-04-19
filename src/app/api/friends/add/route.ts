8aaa7cdec2e5b5b63b716fcdf34da07f97a494c9
ExoskyCode <exoskyv2@gmail.com>
2023-04-19 17:27:54 +0200

    🧿api endpoint add friends

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,11 +1,9 @@
-import { fetchRedis } from '@/helpers/redis'
-import { authOptions } from '@/lib/auth'
-import { db } from '@/lib/db'
-import { pusherServer } from '@/lib/pusher'
-import { toPusherKey } from '@/lib/utils'
-import { addFriendValidator } from '@/lib/validations/add-friend'
-import { getServerSession } from 'next-auth'
-import { z } from 'zod'
+import {addFriendValidator} from "@/lib/validations/add-friend";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
+import {fetchRedis} from "@/helpers/redis";
+import {db} from "@/lib/db";
+import {z} from "zod"
 
 export async function POST(req: Request) {
     try {
@@ -13,67 +11,39 @@ export async function POST(req: Request) {
 
         const { email: emailToAdd } = addFriendValidator.parse(body.email)
 
-        const idToAdd = (await fetchRedis(
-            'get',
-            `user:email:${emailToAdd}`
-        )) as string
-
-        if (!idToAdd) {
-            return new Response("Ce compte n'existe pas", { status: 400 })
-        }
+        const idToAdd = await fetchRedis('get', `user:email:${emailToAdd}`) as string
 
         const session = await getServerSession(authOptions)
 
-        if (!session) {
-            return new Response('Non autorisé', { status: 401 })
-        }
+        if(!session) return new Response('Non autorisé', { status: 401 })
 
-        if (idToAdd === session.user.id) {
-            return new Response('Vous ne pouvez pas vous ajouter vous même', {
-                status: 400,
-            })
-        }
+        /* On vérifie si l'user existe */
 
-        // check if user is already added
-        const isAlreadyAdded = (await fetchRedis(
-            'sismember',
-            `user:${idToAdd}:incoming_friend_requests`,
-            session.user.id
-        )) as 0 | 1
+        if(!idToAdd) return new Response("Cette personne n'existe pas", { status: 400 })
 
-        if (isAlreadyAdded) {
-            return new Response('Vous avez déjà envoyé une demande à cette personne', { status: 400 })
-        }
+        /* On vérifie si l'user ne s'ajoute pas lui même */
 
-        // check if user is already added
-        const isAlreadyFriends = (await fetchRedis(
-            'sismember',
-            `user:${session.user.id}:friends`,
-            idToAdd
-        )) as 0 | 1
+        if(idToAdd === session.user.id) return new Response("Vous ne pouvez pas vous ajouter vous même", { status: 400 })
 
-        if (isAlreadyFriends) {
-            return new Response('Vous avez déjà cette personne en ami', { status: 400 })
-        }
+        const isAlreadyAdded = (await fetchRedis('sismember', `user:${idToAdd}:incoming_friend_requests`, session.user.id)) as 0 | 1
 
-        // valid request, send friend request
+        /* On vérifie si l'user n'a pas déjà envoyé une demande */
 
-        await pusherServer.trigger(
-            toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
-            'incoming_friend_requests',
-            {
-                senderId: session.user.id,
-                senderEmail: session.user.email,
-            }
-        )
+        if(isAlreadyAdded) return new Response("Vous avez déjà envoyé une demande d'ami à cette personne", { status: 400 })
+
+        const isAlreadyFriends = (await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)) as 0 | 1
+
+        /* On vérifie si l'user n'est pas déjà ami avec la personne */
+
+        if(isAlreadyFriends) return new Response("Vous êtes déjà ami avec cette personne", { status: 400 })
+
+        /* On envoie la demande d'ami */
 
         await db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
 
         return new Response('OK')
     } catch (error) {
-        if (error instanceof z.ZodError) {
-            return new Response('Mauvais corps de requête', { status: 422 })
-        }
+        if(error instanceof z.ZodError) return new Response('Mauvais corps de requête', { status: 422 })
 
         return new Response('Mauvaise requête', { status: 400 })
     }
