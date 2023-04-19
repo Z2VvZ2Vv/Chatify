87b999ae2aeaccd1ce6ac81a2873bd1eab3ae5b0
ExoskyCode <exoskyv2@gmail.com>
2023-04-19 23:45:19 +0200

    🧵auth lib

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { UpstashRedisAdapter } from '@next-auth/upstash-redis-adapter';
 import { db } from '@/lib/db';
+import {fetchRedis} from "@/helpers/redis";
 
 export const authOptions: NextAuthOptions = {
 	adapter: UpstashRedisAdapter(db),
@@ -26,12 +27,19 @@ export const authOptions: NextAuthOptions = {
 	],
 	callbacks: {
 		async jwt({ token, user }) {
-			const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+			const dbUserResult = (await fetchRedis('get', `user:${token.id}`)) as
+				| string
+				| null
 
-			if (!dbUser) {
-				token.id = user!.id;
-				return token;
+			if (!dbUserResult) {
+				if (user) {
+					token.id = user!.id
+				}
+
+				return token
 			}
+
+			const dbUser = JSON.parse(dbUserResult) as User
 
 			return {
 				id: dbUser.id,
