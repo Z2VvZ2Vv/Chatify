b50be532f60090e85d15a7b0905755decc2ede23
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 12:08:54 +0200

    ♻️ [REFACTOR] next-auth.d.ts modules

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,12 +6,12 @@ type UserId = string;
 declare module 'next-auth/jwt' {
 	interface JWT  {
 		id: UserId;
-	};
+	}
 }
 declare module 'next-auth' {
 	interface Session {
 		user: User & {
 			id: UserId;
 		};
-	};
+	}
 }
