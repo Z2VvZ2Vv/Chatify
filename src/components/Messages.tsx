14596d601f211cc1104a902e14a9f55d5b10530e
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 11:22:02 +0200

    💄 [FRONT] Messages component

---
+++ src/components/Messages.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { pusherClient } from '@/lib/pusher'
+import { cn, toPusherKey } from '@/lib/utils'
+import { Message } from '@/lib/validations/message'
+import { format } from 'date-fns'
+import { useEffect, useRef, useState } from 'react'
+
+type MessagesProps = {
+    initialMessages: Message[]
+    sessionId: string
+    ChatID: string
+    chatPartner: User
+}
+
+const Messages = ({ initialMessages, sessionId, ChatID }: MessagesProps) => {
+    const [messages, setMessages] = useState<Message[]>(initialMessages)
+
+    const scrollDownRef = useRef<HTMLDivElement | null>(null)
+
+    const formatTimestamp = (timestamp: number) => {
+        return format(timestamp, 'HH:mm')
+    }
+
+    return (
+        <div
+            ref={scrollDownRef}
+            id='messages'
+            className='flex h-full flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto'>
+        <div/>
+
+            {messages.map((message, index) => {
+                const isCurrentUser = message.senderId === sessionId
+
+                const hasNextMessageFromSameUser =
+                    messages[index - 1]?.senderId === messages[index].senderId
+
+                return (
+                    <div
+                        className='chat-message'
+                        key={`${message.id}-${message.timestamp}`}>
+                        <div
+                            className={cn('flex items-end', {
+                                'justify-end': isCurrentUser,
+                            })}>
+                            <div
+                                className={cn(
+                                    'flex flex-col space-y-2 text-base max-w-xs mx-2',
+                                    {
+                                        'order-1 items-end': isCurrentUser,
+                                        'order-2 items-start': !isCurrentUser,
+                                    }
+                                )}>
+                <span
+                    className={cn('px-4 py-2 rounded-lg inline-block', {
+                        'bg-indigo-600 text-white': isCurrentUser,
+                        'bg-gray-200 text-gray-900': !isCurrentUser,
+                        'rounded-br-none':
+                            !hasNextMessageFromSameUser && isCurrentUser,
+                        'rounded-bl-none':
+                            !hasNextMessageFromSameUser && !isCurrentUser,
+                    })}>
+                  {message.text}{' '}
+                    <span className='ml-2 text-xs text-gray-400'>
+                        {formatTimestamp(message.timestamp)}
+                    </span>
+                </span>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Messages
