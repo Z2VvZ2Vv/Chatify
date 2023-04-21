5853063c3041e857fdf162c5b66e2c0c26c1d8a1
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 11:22:55 +0200

    💄 [FRONT] Chat page

--- src/app/(dashboard)/dashboard/chat/[ChatID]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[ChatID]/page.tsx
@@ -4,20 +4,22 @@ import {notFound} from "next/navigation";
 import {db} from "@/lib/db";
 import {fetchRedis} from "@/helpers/redis";
 import {messageArraySchema} from "@/lib/validations/message";
+import Messages from "@/components/Messages";
+import ChatInput from "@/components/ChatInput";
 
 
 type ChatProps = {
     params: {
-        chatId: string
+        ChatID: string
     }
 }
 
 
-async function getChatMessages(chatId: string) {
+async function getChatMessages(ChatID: string) {
     try {
         const results: string[] = await fetchRedis(
             'zrange',
-            `chat:${chatId}:messages`,
+            `chat:${ChatID}:messages`,
             0,
             -1
         )
@@ -32,22 +34,32 @@ async function getChatMessages(chatId: string) {
     }
 }
 const page = async ({params}: ChatProps) => {
-    const { chatId } = params
+    const { ChatID } = params
     const session = await getServerSession(authOptions)
 
     if(!session) notFound()
 
     const { user } = session
-    const [userId1, userId2] = chatId.split('--')
+    const [userId1, userId2] = ChatID.split('--')
 
     if(user.id !== userId1 && user.id !== userId2) notFound()
 
     const chatPartnerId = user.id === userId1 ? userId2 : userId1
     const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User
-    const initialMessages = await getChatMessages(chatId)
+    const initialMessages = await getChatMessages(ChatID)
 
     return (
-        <h1>{params.chatId}</h1>
+        <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-4rem)]">
+            <div className={"flex sm:items-center justify-between py-5 border-b-2 border-gray-500"}>
+                    <div className={"flex flex-col leading-tight"}>
+                        <div className={"text-md ml-8 flex items-center"}>
+                            <span className={"text-gray-50 font-medium"}>{chatPartner.email}</span>
+                        </div>
+                    </div>
+            </div>
+            <Messages sessionId={session.user.id} initialMessages={initialMessages} chatPartner={chatPartner} ChatID={""} />
+            <ChatInput chatPartner={chatPartner} ChatID={ChatID}/>
+        </div>
     )
 }
 
