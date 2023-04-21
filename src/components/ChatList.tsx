82054fd439e73a5bcc532d92c899af65d7dd8304
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 18:49:53 +0200

    💄 [FRONT] chatList real time

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,25 +1,77 @@
 'use client'
 
-import {useEffect, useState} from "react";
-import {usePathname} from "next/navigation";
-import {chatHrefConstructor} from "@/lib/utils";
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast'
+
+import { chatHrefConstructor, toPusherKey } from '@/lib/utils'
+import { pusherClient } from '@/lib/pusher'
+
+import UnseenChat from "@/components/UnseenChat";
 
 type ChatListProps = {
     friends: User[],
     sessionId: string
 }
-const ChatList = ({friends, sessionId}: ChatListProps) => {
-    const [unseenMessages, setUnseenMessages] = useState<Message[]>([])
 
-    const pathName = usePathname()
+type MessageType = {
+    senderEmail: string
+} & Message
+const ChatList = ({friends, sessionId}: ChatListProps) => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const [unseenMessages, setUnseenMessages] = useState<Message[]>([])
+    const [activeChats, setActiveChats] = useState<User[]>(friends)
 
     useEffect(() => {
-        if(pathName?.includes('chat')) {
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`))
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
+
+        const newFriendHandler = (newFriend: User) => {
+            console.log("Nouvel ami", newFriend)
+            setActiveChats((prev) => [...prev, newFriend])
+        }
+
+        const chatHandler = (message: MessageType) => {
+            const shouldNotify =
+                pathname !==
+                `/dashboard/chat/${chatHrefConstructor(sessionId, message.senderId)}`
+
+            if (!shouldNotify) return
+
+            // should be notified
+            toast.custom((toast) => (
+                <UnseenChat
+                    sessionId={sessionId}
+                    senderId={message.senderId}
+                    senderEmail={message.senderEmail}
+                    senderMessage={message.text}
+                     notif={toast}
+                />
+            ))
+
+            setUnseenMessages((prev) => [...prev, message])
+        }
+
+        pusherClient.bind('new_message', chatHandler)
+        pusherClient.bind('new_friend', newFriendHandler)
+
+        return () => {
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:chats`))
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
+
+            pusherClient.unbind('new_message', chatHandler)
+            pusherClient.unbind('new_friend', newFriendHandler)
+        }
+    }, [pathname, sessionId, router])
+
+    useEffect(() => {
+        if (pathname?.includes('chat')) {
             setUnseenMessages((prev) => {
-                return prev.filter((message) => !pathName.includes(message.senderId))
+                return prev.filter((msg) => !pathname.includes(msg.senderId))
             })
         }
-    },[pathName])
+    }, [pathname])
 
     return (
         <ul role={"list"} className={"max-h[25rem] oveerflow-y-auto -mx-2 space-y-1"}>
@@ -31,9 +83,7 @@ const ChatList = ({friends, sessionId}: ChatListProps) => {
                         <a className={"truncate hover:text-indigo-600 hover:bg-gray-50 flex items-center group gap-x-3 rounded-md p-2 text-xs leading-6 font-semibold"} href={`/dashboard/chat/${chatHrefConstructor(sessionId, friend.id)}`}>
                             {friend.email}
                             {unseenMessagesCount > 0 ? (
-                                <div className={"bg-indigo-600 font-medium text-white w-4 h-4 text-xs rounded-full justify)center items-center"}>
-                                    {unseenMessagesCount}
-                                </div>
+                                <div className={"rounded-full w-5 h-5 text-xs flex justify-center items-center bg-indigo-600 text-white"}>{unseenMessagesCount}</div>
                             ): null}
                         </a>
                     </li>
