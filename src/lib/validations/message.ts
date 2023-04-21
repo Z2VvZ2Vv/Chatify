0a7e76b67fb29149796ad69b3f34599fdf71d5cc
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 11:05:31 +0200

    🌀 [TYPES] Update message schema text max length of 600 chr

--- src/lib/validations/message.ts
+++ src/lib/validations/message.ts
@@ -5,7 +5,7 @@ import {z} from "zod";
 export const messageSchema = z.object({
     id: z.string(),
     senderId: z.string(),
-    text: z.string(),
+    text: z.string().max(600, "Must be less than 600 characters"),
     timestamp: z.number()
 })
 
