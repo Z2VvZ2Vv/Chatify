ce146a4f391025b924ca9a54711cb815f7d8177e
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 18:49:22 +0200

    💄 [FRONT] friendRequests real time

--- src/components/FriendRequestsSideBarOptions.tsx
+++ src/components/FriendRequestsSideBarOptions.tsx
@@ -2,7 +2,9 @@
 
 import Link from "next/link";
 import {User} from "lucide-react";
-import {useState} from "react";
+import { pusherClient } from '@/lib/pusher'
+import { toPusherKey } from '@/lib/utils'
+import {useState, useEffect} from "react";
 
 type FriendRequestsSideBarOptionsProps = {
     sessionId: string,
@@ -10,7 +12,37 @@ type FriendRequestsSideBarOptionsProps = {
 }
 
 const FriendRequestsSideBarOptions = ({sessionId, initialUnseenRequestCount}: FriendRequestsSideBarOptionsProps) => {
-    const [unseenRequestCount, setUnseenRequestCount] = useState<number>(initialUnseenRequestCount);
+    const [unseenRequestCount, setUnseenRequestCount] = useState<number>(
+        initialUnseenRequestCount
+    )
+
+    useEffect(() => {
+        pusherClient.subscribe(
+            toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+        )
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
+
+        const friendRequestHandler = () => {
+            setUnseenRequestCount((prev) => prev + 1)
+        }
+
+        const addedFriendHandler = () => {
+            setUnseenRequestCount((prev) => prev - 1)
+        }
+
+        pusherClient.bind('incoming_friend_requests', friendRequestHandler)
+        pusherClient.bind('new_friend', addedFriendHandler)
+
+        return () => {
+            pusherClient.unsubscribe(
+                toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+            )
+            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
+
+            pusherClient.unbind('new_friend', addedFriendHandler)
+            pusherClient.unbind('incoming_friend_requests', friendRequestHandler)
+        }
+    }, [sessionId])
     return (
         <Link href={'dashboard/requests'} className={'hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'}>
             <div className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group:hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
