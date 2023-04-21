c0cbcabbe2a3abe53814a669e6e56bb913d790d9
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 08:55:03 +0200

    💄 [FRONT] Show friends list

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -7,6 +7,8 @@ import { Icons, Icon } from "@/components/Icons";
 import SignOutButton from "@/components/SignOutButton";
 import FriendRequestsSideBarOptions from "@/components/FriendRequestsSideBarOptions";
 import {fetchRedis} from "@/helpers/redis";
+import {getFriendsByUserId} from "@/helpers/getFriendsByUserId";
+import ChatList from "@/components/ChatList";
 
 type LayoutProps = {
 	children: ReactNode;
@@ -31,6 +33,8 @@ const Layout = async ({ children }: LayoutProps) => {
 	const session = await getServerSession(authOptions);
 	if (!session) notFound();
 
+	const friends = await getFriendsByUserId(session.user.id);
+
 	const unseenRequestCount = (
 		(await fetchRedis(
 			'smembers',
@@ -42,12 +46,14 @@ const Layout = async ({ children }: LayoutProps) => {
 		<div className='w-full flex h-screen'>
 			<div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto bg-neutral-950 px-6'>
 				<Link href={'/dashboard'} className="flex h-16 shrink-0 items-center">Chatify</Link>
-				<div className="text-xs font-semibold leading-6 text-gray-400">
-					Vos conversations
-				</div>
+
+					{friends.length > 0 ? (
+						<div className="text-xs font-semibold leading-6 text-gray-400">Vos conversations</div>
+					): null}
+
 				<nav className="flex flex-1 flex-col">
 					<ul role={"list"} className={"flex flex-1 flex-col gap-y-7"}>
-						<li>LoremLoremLoremLoremLorem</li>
+						<li><ChatList sessionId={session.user.id} friends={friends} /></li>
 						<li>
 							<div className={"text-xs font-semibold leading-6 text-gray-400"}>Overview</div>
 							<ul role={"list"} className={"-mx-2 mt-2 space-y-1"}>
@@ -58,7 +64,7 @@ const Layout = async ({ children }: LayoutProps) => {
 											<Link
 												href={option.href}
 												className='hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'>
-                        <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+							<span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
                           <Icon className='h-5 w-5' />
                         </span>
 
@@ -67,12 +73,12 @@ const Layout = async ({ children }: LayoutProps) => {
 										</li>
 									)
 								})}
+							<li>
+								<FriendRequestsSideBarOptions sessionId={session.user.id} initialUnseenRequestCount={unseenRequestCount} />
+							</li>
 							</ul>
 						</li>
 
-						<li>
-							<FriendRequestsSideBarOptions sessionId={session.user.id} initialUnseenRequestCount={unseenRequestCount} />
-						</li>
 
 						<li className={"-mx-6 mt-auto flex items-center"}>
 							<div className="flex flex-1 items-center gap-x-4 px-6 py-6 text-sm font-semibold leading-6">
