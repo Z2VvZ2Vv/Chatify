c983a6c5b9d626913048fd9d56b9610f60a288e9
ExoskyCode <exoskyv2@gmail.com>
2023-04-21 12:09:32 +0200

    🌀 [TYPES] receiverId added to messageSchema

--- src/lib/validations/message.ts
+++ src/lib/validations/message.ts
@@ -1,10 +1,10 @@
-// @ts-ignore
 import {z} from "zod";
 
 
 export const messageSchema = z.object({
     id: z.string(),
     senderId: z.string(),
+    receiverId: z.string(),
     text: z.string().max(600, "Must be less than 600 characters"),
     timestamp: z.number()
 })
