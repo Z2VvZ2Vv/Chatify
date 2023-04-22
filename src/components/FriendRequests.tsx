a1182727f0a4467f7b7d265ad216e5af62881565
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 10:16:55 +0200

    💄 [FRONT] mini correction friendRequest

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -74,7 +74,7 @@ const FriendRequests = ({sessionId, incomingFriendRequests}: FriendRequestsProps
                         onClick={() => acceptFriend(request.senderId)}>
                        <UserPlus className={'text-white'}/>
                        <p className={'font-medium text-lg'}>{request.senderEmail}</p>
-                       <button aria-label={"accept friend"} className={'w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md'}>
+                       <button aria-label={"accept friend"} className={'w-8 h-8 bg-[#10A37F] hover:bg-[#16bd93] grid place-items-center rounded-full transition hover:shadow-md'}>
                            <Check className={'font-semibold text-white w-3/4 h-3/4'} />
                        </button>
                        <button aria-label={"deny friend"} className={'w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'}
