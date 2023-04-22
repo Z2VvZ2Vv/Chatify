300f59e77375b155f9333fd3174de5c93e910789
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 10:17:07 +0200

    💄 [FRONT] mini correction FriendRequestsSideBarOptions

--- src/components/FriendRequestsSideBarOptions.tsx
+++ src/components/FriendRequestsSideBarOptions.tsx
@@ -44,14 +44,14 @@ const FriendRequestsSideBarOptions = ({sessionId, initialUnseenRequestCount}: Fr
         }
     }, [sessionId])
     return (
-        <Link href={'dashboard/requests'} className={'hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'}>
-            <div className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group:hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
-                <User className="h-5 w-5" />
+        <Link href={'dashboard/requests'} className={'hover:text-[#10A37F] hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'}>
+            <div className="text-gray-400 border-gray-200 group-hover:border-[#10A37F] group:hover:text-[#10A37F] flex h-8 w-8 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                <User className="h-6 w-6" />
             </div>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <p className={'truncate'}>Demandes d'ami</p>
+            <p className={'truncate mt-0.5'}>Demandes d'ami</p>
             {unseenRequestCount > 0 ? (
-                <div className={"rounded-full w-5 h-5 text-xs flex justify-center items-center bg-indigo-600 text-white"}>{unseenRequestCount}</div>
+                <div className={"rounded-full w-5 h-5 text-xs flex justify-center items-center bg-[#10A37F] text-white"}>{unseenRequestCount}</div>
             ): null}
         </Link>
     )
