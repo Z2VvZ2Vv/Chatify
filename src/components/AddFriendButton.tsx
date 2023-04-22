15c0dd864dba60188b492c592e2762c61531e5d0
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 14:13:14 +0200

    ♻️ [REFACTOR] Form Handler + toast error and success friend request

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -2,28 +2,22 @@
 
 import { addFriendValidator } from '@/lib/validations/add-friend'
 import axios, { AxiosError } from 'axios'
-import { FC, useState } from 'react'
+import React, { FC, useState } from 'react'
 import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
 import {Button} from "@/components/ui/Button";
-import {useForm} from "react-hook-form";
+import {toast} from "react-hot-toast";
 
-interface AddFriendButtonProps {}
-
-type FormData = z.infer<typeof addFriendValidator>
-
-const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+const AddFriendButton = () => {
     const [showSuccessState, setShowSuccessState] = useState<boolean>(false)
 
-    const {
-        register,
-        handleSubmit,
-        setError,
-        formState: { errors },
-    } = useForm<FormData>({
-        resolver: zodResolver(addFriendValidator),
-    })
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (!event) return
+        event.preventDefault()
 
+        const data = new FormData(event.currentTarget)
+        const email = String(data.get('email'))
+        onSubmit(email)
+    }
     const addFriend = async (email: string) => {
         try {
             const validatedEmail = addFriendValidator.parse({ email })
@@ -31,29 +25,32 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
             await axios.post('/api/friends/add', {
                 email: validatedEmail,
             })
-
             setShowSuccessState(true)
+
         } catch (error) {
+            setShowSuccessState(false)
             if (error instanceof z.ZodError) {
-                setError('email', { message: error.message })
+                toast.error(error.message)
                 return
             }
 
             if (error instanceof AxiosError) {
-                setError('email', { message: error.response?.data })
+                toast.error(error.response?.data)
                 return
             }
 
-            setError('email', { message: 'Une erreur est survenue' })
+            toast.error('Une erreur est survenue')
         }
     }
 
-    const onSubmit = (data: FormData) => {
-        addFriend(data.email)
+    const onSubmit = (email: string) => {
+        addFriend(email)
     }
 
+    if(showSuccessState) setShowSuccessState(false)
+
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className='w-96 m-auto'>
+        <form onSubmit={(event) => handleSubmit(event)} className='w-96 m-auto'>
             <label
                 htmlFor='email'
                 className='block text-sm font-medium leading-6'>
@@ -62,17 +59,15 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
             <div className='mt-2 flex flex-col gap-4'>
                 <input
-                    {...register('email')}
+                    name='email'
                     type='text'
                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#10A37F] sm:text-sm sm:leading-6'
                     placeholder='toi@example.com'
                 />
                 <Button>Ajouter</Button>
             </div>
-            <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>
             {showSuccessState ? (
-                // eslint-disable-next-line react/no-unescaped-entities
-                <p className='mt-1 text-sm text-green-600'>Demande d'ami envoyée</p>
+                toast.success('Demande d\'ami envoyée')
             ) : null}
         </form>
     )
