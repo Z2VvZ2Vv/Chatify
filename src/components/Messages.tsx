b0f41082afe56a0dc17dd8b460dcab4100009c23
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 10:18:17 +0200

    💄 [FRONT] Small UI fix in messages component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -71,7 +71,7 @@ const Messages = ({ initialMessages, sessionId, ChatID }: MessagesProps) => {
                                 )}>
                 <span
                     className={cn('px-4 py-2 rounded-lg inline-block max-w-[50vw] overflow-hidden break-words', {
-                        'bg-indigo-600 text-white': isCurrentUser,
+                        'bg-[#10A37F] text-white': isCurrentUser,
                         'bg-gray-200 text-gray-900': !isCurrentUser,
                         'rounded-br-none':
                             !hasNextMessageFromSameUser && isCurrentUser,
