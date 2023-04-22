9323670d300dd2445bae5f4fe41897a0992e8cb2
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 10:16:14 +0200

    ♻️[REFACTOR] eslint-disable-next-line react/no-unescaped-entities

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -64,14 +64,15 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
                 <input
                     {...register('email')}
                     type='text'
-                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                    placeholder='you@example.com'
+                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#10A37F] sm:text-sm sm:leading-6'
+                    placeholder='toi@example.com'
                 />
                 <Button>Ajouter</Button>
             </div>
             <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>
             {showSuccessState ? (
-                <p className='mt-1 text-sm text-green-600'>Demmande d'ami envoyée</p>
+                // eslint-disable-next-line react/no-unescaped-entities
+                <p className='mt-1 text-sm text-green-600'>Demande d'ami envoyée</p>
             ) : null}
         </form>
     )
