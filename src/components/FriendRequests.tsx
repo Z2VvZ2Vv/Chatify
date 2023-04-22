f2ab9c1b1bc4ad841916112c8a74b8619e7fad75
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 15:48:32 +0200

    ♻️ [REFACTOR] FriendRequests component

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -6,6 +6,7 @@ import { toPusherKey } from '@/lib/utils'
 import { pusherClient } from '@/lib/pusher'
 import axios from "axios";
 import {useRouter} from "next/navigation";
+import {toast} from "react-hot-toast";
 
 type FriendRequestsProps = {
     incomingFriendRequests: IncomingFriendRequest[],
@@ -21,13 +22,13 @@ const FriendRequests = ({sessionId, incomingFriendRequests}: FriendRequestsProps
         pusherClient.subscribe(
             toPusherKey(`user:${sessionId}:incoming_friend_requests`)
         )
-        console.log("listening to ", `user:${sessionId}:incoming_friend_requests`)
+        console.log("A l'écoute de: ", `user:${sessionId}:incoming_friend_requests`)
 
         const friendRequestHandler = ({
                                           senderId,
                                           senderEmail,
                                       }: IncomingFriendRequest) => {
-            console.log("function got called")
+            console.log("Fonction appelée")
             setFriendRequests((prev) => [...prev, { senderId, senderEmail }])
         }
 
@@ -42,39 +43,47 @@ const FriendRequests = ({sessionId, incomingFriendRequests}: FriendRequestsProps
     }, [sessionId])
 
     const acceptFriend = async (senderId: string) => {
-        await axios.post('/api/friends/accept', { id: senderId })
+        try {
+            await axios.post('/api/friends/accept', {id: senderId})
 
-        setFriendRequests((prev) =>
-            prev.filter((request) => request.senderId !== senderId)
-        )
+            setFriendRequests((prev) =>
+                prev.filter((request) => request.senderId !== senderId)
+            )
 
-        router.refresh()
+            router.refresh()
+        } catch (error) {
+            toast.error('Une erreur est survenue')
+        }
     }
 
     const denyFriend = async (senderId: string) => {
-        await axios.post('/api/friends/deny', { id: senderId })
+        try {
+            await axios.post('/api/friends/deny', {id: senderId})
 
-        setFriendRequests((prev) =>
-            prev.filter((request) => request.senderId !== senderId)
-        )
+            setFriendRequests((prev) =>
+                prev.filter((request) => request.senderId !== senderId)
+            )
 
-        router.refresh()
+            router.refresh()
+            router.push('/dashboard/requests')
+        } catch (error) {
+            toast.error('Une erreur est survenue')
+        }
     }
-
 
     return (
 
        <>
            {friendRequests.length === 0 ? (
                // eslint-disable-next-line react/no-unescaped-entities
-               <p className={'text-sm'}>Aucune demande d'ami</p>
+               <p className={'text-sm'}>Aucune demande d'amis</p>
            ): (
                friendRequests.map((request) => (
                    <div key={request.senderId} className={'flex gap-4 items-center'}
-                        onClick={() => acceptFriend(request.senderId)}>
+                        >
                        <UserPlus className={'text-white'}/>
                        <p className={'font-medium text-lg'}>{request.senderEmail}</p>
-                       <button aria-label={"accept friend"} className={'w-8 h-8 bg-[#10A37F] hover:bg-[#16bd93] grid place-items-center rounded-full transition hover:shadow-md'}>
+                       <button onClick={() => acceptFriend(request.senderId)} aria-label={"accept friend"} className={'w-8 h-8 bg-[#10A37F] hover:bg-[#16bd93] grid place-items-center rounded-full transition hover:shadow-md'}>
                            <Check className={'font-semibold text-white w-3/4 h-3/4'} />
                        </button>
                        <button aria-label={"deny friend"} className={'w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'}
