36c261f34e5cfb952f9a95c13ca491df58aa1955
ExoskyCode <exoskyv2@gmail.com>
2023-04-22 15:36:06 +0200

    💄️ [BACK] Find if friend is denied or not

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -8,6 +8,7 @@ import { chatLinkBuilder, toPusherKey } from '@/lib/utils'
 import { pusherClient } from '@/lib/pusher'
 
 import UnseenChat from "@/components/UnseenChat";
+import {fetchRedis} from "@/helpers/redis";
 
 type ChatListProps = {
     friends: User[],
@@ -23,11 +24,15 @@ const ChatList = ({friends, sessionId}: ChatListProps) => {
     const [unseenMessages, setUnseenMessages] = useState<Message[]>([])
     const [activeChats, setActiveChats] = useState<User[]>(friends)
 
+
+
     useEffect(() => {
         pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`))
         pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
 
-        const newFriendHandler = (newFriend: User) => {
+        const newFriendHandler = async (newFriend: User) => {
+            const friendList = await fetchRedis('smembers', `user:${sessionId}:friends`) as string[]
+            const isFriend = friendList.includes(newFriend.id || newFriend.email)
             console.log("Nouvel ami", newFriend)
             setActiveChats((prev) => [...prev, newFriend])
         }
@@ -40,13 +45,13 @@ const ChatList = ({friends, sessionId}: ChatListProps) => {
             if (!shouldNotify) return
 
             // should be notified
-            toast.custom((toast) => (
+            toast.custom((t) => (
                 <UnseenChat
                     sessionId={sessionId}
                     senderId={message.senderId}
                     senderEmail={message.senderEmail}
                     senderMessage={message.text}
-                     notif={toast}
+                    notif={t}
                 />
             ))
 
@@ -74,21 +79,28 @@ const ChatList = ({friends, sessionId}: ChatListProps) => {
     }, [pathname])
 
     return (
-        <ul role={"list"} className={"max-h[25rem] oveerflow-y-auto -mx-2 space-y-1"}>
-            {friends.sort().map((friend) => {
-                const unseenMessagesCount = unseenMessages.filter((unseenMessage) => {
-                    return unseenMessage.senderId === friend.id}).length
-                return (
-                    <li key={friend.id}>
-                        <a className={"truncate hover:text-[#10A37F] hover:bg-gray-50 flex items-center group gap-x-3 rounded-md p-2 text-xs leading-6 font-semibold"} href={`/dashboard/chat/${chatLinkBuilder(sessionId, friend.id)}`}>
-                            {friend.email}
-                            {unseenMessagesCount > 0 ? (
-                                <div className={"rounded-full w-5 h-5 text-xs flex justify-center items-center bg-[#10A37F] text-white"}>{unseenMessagesCount}</div>
-                            ): null}
-                        </a>
-                    </li>
-                )
-            })}
+        <ul role={"list"} className={"max-h-[25rem] overflow-y-auto space-y-1"}>
+
+            <div className="text-xs mt-12 font-semibold leading-6 text-gray-400">Vos conversations</div>
+
+            {activeChats.length > 0 ?
+                (activeChats.sort().map((friend) => {
+                    const unseenMessagesCount = unseenMessages.filter((unseenMsg) => {
+                        return unseenMsg.senderId === friend.id
+                    }).length
+
+                    return (
+                        <li key={friend.id}>
+                            <a className={"truncate hover:text-[#10A37F] hover:bg-gray-50 flex items-center group gap-x-3 rounded-md p-2 text-xs leading-6 font-semibold"} href={`/dashboard/chat/${chatLinkBuilder(sessionId, friend.id)}`}>
+                                {friend.email}
+                                {unseenMessagesCount > 0 ? (
+                                    <div className={"rounded-full w-5 h-5 text-xs flex justify-center items-center bg-[#10A37F] text-white"}>{unseenMessagesCount}</div>
+                                ): null}
+                            </a>
+                        </li>
+                    )
+                }))
+            : null}
         </ul>
     )
 }
